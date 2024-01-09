e77814581f0fc25ef76c1ad5ffbe147f55d52621
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2024-01-09 10:38:01 +0100

    Fix updater script

--- update-zones.sh
+++ update-zones.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p iprange curl
+#!nix-shell -i bash -p iprange whois
 
-url="https://www.cc2asn.com/data/"
+url="cc2asn.com"
 
 
 echo "Download and & Shrink zone files..."
 
-for cc in `ls ipv4 | sed 's/.zone//'`; do
+for cc in $(ls ipv4 | sed 's/.zone//'); do
   echo -n "$cc "
-  curl -s ${url}${cc}_ipv4 | iprange > ipv4/${cc}.zone
-  curl -s ${url}${cc}_ipv6 | sort > ipv6/${cc}.zone
+  # ipv4/6 has bug, request all instead
+  whois -h "$url" all "$cc" | sed '/^A/d;/:/d' | iprange > "ipv4/${cc}.zone"
+  whois -h "$url" all "$cc" | sed '/^A/d;/\./d' | sort > "ipv6/${cc}.zone"
 done
 echo ""
 
